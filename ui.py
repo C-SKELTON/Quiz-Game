c00cc55c25b7c7fe55bf036c8d1babbacee33f1b
C-SKELTON <101684635+C-SKELTON@users.noreply.github.com>
2022-03-20 12:57:03 -0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -11,26 +11,29 @@ class QuizInterface:
 
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(pady=20, padx=20, background=THEME_COLOR)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150, 150, width= 280, text="Some Question Text", fill=THEME_COLOR,
-                                                     font=("arial", 20, "italic"))
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
-        self.logo_img = PhotoImage(file="true.png")
-        self.logo_img2 = PhotoImage(file="false.png")
+        true_image = PhotoImage(file="true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed,borderwidth=0)
+        self.true_button.grid(row=2, column=0)
 
-        self.score_text = Label(text="Score: 0", bg=THEME_COLOR, fg="white", font=("arial", 12))
-        self.score_text.grid(column=1, row=0, pady=20)
-
-        self.check_button = Button(image=self.logo_img, background=THEME_COLOR, highlightthickness=0, borderwidth=0
-                                   , command=self.true_pressed)
-        self.check_button.grid(column=0, row=2, pady=30)
-
-        self.x_button = Button(image=self.logo_img2, background=THEME_COLOR, highlightthickness=0, borderwidth=0,
-                               command= self.false_pressed)
-        self.x_button.grid(column=1, row=2, pady=30)
-
+        false_image = PhotoImage(file="false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed, borderwidth=0)
+        self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
 
@@ -39,7 +42,7 @@ class QuizInterface:
     def get_next_question(self):
         self.canvas.config(bg="white")
         if self.quiz.still_has_questions():
-            self.score_text.config(text=f"Score: {self.quiz.score}")
+            self.score_label.config(text=f"Score: {self.quiz.score}")
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
         else:
@@ -47,15 +50,24 @@ class QuizInterface:
             self.true_button.config(state="disabled")
             self.false_button.config(state="disabled")
 
-
     def true_pressed(self):
         self.give_feedback(self.quiz.check_answer("True"))
+
     def false_pressed(self):
-        self.give_feedback(self.quiz.check_answer("False"))
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
 
     def give_feedback(self, is_right):
-        if is_right: #if True/False matches the wuestion answer then change background color
+        if is_right:
             self.canvas.config(bg="green")
         else:
             self.canvas.config(bg="red")
         self.window.after(1000, self.get_next_question)
+
+
+
+
+
+
+
+
