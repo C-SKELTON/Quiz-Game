7ae447cf9a1eab74f5d4b0a6f212eea1bb03bca4
C-SKELTON <101684635+C-SKELTON@users.noreply.github.com>
2022-03-20 12:52:21 -0400

    Update data.py

--- data.py
+++ data.py
@@ -4,10 +4,8 @@ parameters = {
     "amount": 10,
     "type": "boolean",
 }
+
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
-questions = data["results"]
-
-
-question_data = questions
+question_data = data["results"]
